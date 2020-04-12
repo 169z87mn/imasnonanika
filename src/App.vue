f189971be9ec64108bb6d7e6d10d5a158b10c422
169z87mn <169z87mn@gmail.com>
2020-04-12 23:08:36 +0900

    とりあえず取得してみた

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloImasWorld />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloImasWorld from './components/HelloImasWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloImasWorld
   }
 }
 </script>
---
+++ src/components/HelloImasWorld.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <b-button v-on:click="getidol">GET!!!</b-button>
+    <hr>
+    {{hei}}
+  </div>
+</template>
+
+<script>
+import idolheight from 'raw-loader!../api/query/heightorder.sparql'
+import axios from 'axios';
+
+export default {
+  name: 'HelloImasWorld',
+  data: () => ({
+    hei: ''
+  }),
+  methods: {
+    getidol() {
+      console.log('開始1')
+      const url = 'https://sparql.crssnky.xyz/spql/imas/query';
+        console.log('開始2')
+        axios.get(url, {
+          params: {
+            query: idolheight
+          }
+        }).then((e) => {
+          this.hei = e;
+        })
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
